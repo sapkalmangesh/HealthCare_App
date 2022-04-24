5a412c73b626a27c23d7bd2f131a53424c74cc3b
sapkalmangesh <sapkalmangesh1990@gmail.com>
2022-04-24 13:57:14 +0530

    test case code updates

--- src/main/java/com/ms/in/entity/Specialization.java
+++ src/main/java/com/ms/in/entity/Specialization.java
@@ -7,11 +7,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name="specialization_tab_col")
+@Table(name="specialization_tab")
 public class Specialization {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/ms/in/specialization/SpecializationRepositoryTest.java
+++ src/test/java/com/ms/in/specialization/SpecializationRepositoryTest.java
@@ -1,11 +1,16 @@
 package com.ms.in.specialization;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import com.ms.in.entity.Specialization;
 import com.ms.in.repo.SpecializationRepository;
 
 @DataJpaTest(showSql = true)
@@ -20,7 +25,13 @@ public class SpecializationRepositoryTest {
 	/***
 	 * 1. Test save operation
 	 */
+	@Test
+	@Order(1)
 	public void specTestCreate() {
+		Specialization spec= new Specialization(null,"CRLDS","Cardiologists",
+												 "They’re experts on the heart and blood vessels.");
+		spec=repo.save(spec);
+		assertNotNull(spec.getSpecId(), "Spec id not created");
 		
 	}
 	
